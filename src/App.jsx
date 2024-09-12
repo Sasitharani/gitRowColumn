cc2bc5e01370c115cad85923b6127f29eda37f06
sasi <sasitharani@gmail.com>
2024-09-12 13:02:22 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-red-50"> 
+      <div className="grid grid-cols-3 border-2 border-gray-500 gap-2">
+        <div className="border-2 border-green-400 h-48 "></div>
+        <div className="border-2 border-green-400 h-48"></div>
+        <div className="border-2 border-green-400 h-48"></div>
+      </div>
+      <div className="grid grid-rows-3 border-2 border-yellow-800 gap-2">
+        <div className="border-2 border-yellow-400 h-11"></div>
+        <div className="border-2 border-yellow-400 h-11"></div>
+        <div className="border-2 border-yellow-400 h-11"></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
