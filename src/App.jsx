3b4b0d069799d4ef6346ca57613a494415b5912b
sasi <sasitharani@gmail.com>
2024-09-12 16:05:51 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
     <>
     <div className="bg-red-50"> 
       <div className="grid grid-cols-3 border-2 border-gray-500 gap-2">
-        <div className="border-2 border-green-400 h-48 "></div>
-        <div className="border-2 border-green-400 h-48"></div>
-        <div className="border-2 border-green-400 h-48"></div>
+        <div className="border-2 border-green-400 h-48 flex  items-center ">grid grid-cols-3</div>
+        <div className="border-2 border-green-400 h-48">grid grid-cols-3</div>
+        <div className="border-2 border-green-400 h-48">grid grid-cols-3</div>
       </div>
       <div className="grid grid-rows-3 border-2 border-yellow-800 gap-2">
-        <div className="border-2 border-yellow-400 h-11"></div>
-        <div className="border-2 border-yellow-400 h-11"></div>
-        <div className="border-2 border-yellow-400 h-11"></div>
+        <div className="border-2 border-yellow-400 h-11">grid-rows-3</div>
+        <div className="border-2 border-yellow-400 h-11">grid-rows-3</div>
+        <div className="border-2 border-yellow-400 h-11">grid-rows-3</div>
       </div>
     </div>
     </>
